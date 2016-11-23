cb26e23c7f10046b3a92e9071aaa7dd363ee0921
alinaalam <alinaalam@hotmail.com>
2016-11-23 16:07:51 +0500

    Removed folder from repository

--- Test1/Main.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-$x = rtrim(fgets(STDIN));
-$y = rtrim(fgets(STDIN));
-
-if ($x < 200 && $y < 200)
-	echo $x + $y;
-?>
--- Test1/Main.py
+++
@@ -1,5 +0,0 @@
-x = input()
-y = input()
-
-if (x < 200 and y < 200):
-	print x+y
